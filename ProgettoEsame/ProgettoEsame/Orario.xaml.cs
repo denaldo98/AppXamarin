b2e66598ac953c02dc400a5b69b9e4e17370fec0
denaldo98 <denaldo98@gmail.com>
2020-06-07 14:52:55 +0200

    fix overflow menu

--- ProgettoEsame/ProgettoEsame/Orario.xaml.cs
+++ ProgettoEsame/ProgettoEsame/Orario.xaml.cs
@@ -20,64 +20,44 @@ namespace ProgettoEsame
 
             ToolbarItem item = new ToolbarItem
             {
-                Text = "Faq",   
-                Order = ToolbarItemOrder.Secondary,
-                Priority = 0
-            };
-
-            item.Clicked += (sender, args) => {
-                Navigation.PushAsync(new FaqPage());
-            };
-
-            // "this" refers to a Page object
-            this.ToolbarItems.Add(item);
-
-            ToolbarItem item2 = new ToolbarItem
-            {
                 Text = "Contatti",
                 IconImageSource = ImageSource.FromFile("example_icon.png"),
                 Order = ToolbarItemOrder.Secondary,
                 Priority = 0
             };
 
-            item2.Clicked += (sender, args) => {
+            item.Clicked += (sender, args) => {
                 Navigation.PushAsync(new ContattiPage());
             };
+            this.ToolbarItems.Add(item);
 
-            // "this" refers to a Page object
-            this.ToolbarItems.Add(item2);
 
-            ToolbarItem item3 = new ToolbarItem
+            ToolbarItem item2 = new ToolbarItem
             {
-                Text = "Item3",
-                IconImageSource = ImageSource.FromFile("example_icon.png"),
+                Text = "Faq",   
                 Order = ToolbarItemOrder.Secondary,
                 Priority = 0
             };
 
-            item3.Clicked += (sender, args) => {
-                Navigation.PushAsync(new Page1());
+            item2.Clicked += (sender, args) => {
+                Navigation.PushAsync(new FaqPage());
             };
-
-            // "this" refers to a Page object
-            this.ToolbarItems.Add(item3);
+            this.ToolbarItems.Add(item2); // "this" refers to a Page object
 
 
-            ToolbarItem item4 = new ToolbarItem
+            ToolbarItem item3 = new ToolbarItem
             {
                 Text = "Logout",
                 IconImageSource = ImageSource.FromFile("example_icon.png"),
                 Order = ToolbarItemOrder.Secondary,
                 Priority = 0
             };
-
-            item4.Clicked += async (sender, args) => {
+            item3.Clicked += async (sender, args) => {
                 bool logout = await Auth.Logout();
                 if (logout)
                 {
                     Application.Current.Properties["logged"] = "false";
                     await Application.Current.SavePropertiesAsync();
-
                     //Application.Current.Properties.Clear();
                     await DisplayAlert("Attenzione!", "Il logout è stato eseguito, l'app verrà chiusa!", "Ok");
                     await System.Threading.Tasks.Task.Delay(1000);
@@ -87,9 +67,7 @@ namespace ProgettoEsame
                 else await DisplayAlert("Attenzione!", "Non è stato possibile effettuare il logout", "Ok");
                
             };
-
-            // "this" refers to a Page object
-            this.ToolbarItems.Add(item4);
+            this.ToolbarItems.Add(item3);
 
 
 
--- ProgettoEsame/ProgettoEsame/View/CorsiPage.xaml.cs
+++ ProgettoEsame/ProgettoEsame/View/CorsiPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ProgettoEsame.ViewModel;
+using ProgettoEsame.ViewModel.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,57 @@ namespace ProgettoEsame.View
             Title = "Corsi";
 
             vm = Resources["vm"] as CorsoVM; //accedo alla risorsa tramite chiave
+
+            ToolbarItem item = new ToolbarItem
+            {
+                Text = "Contatti",
+                IconImageSource = ImageSource.FromFile("example_icon.png"),
+                Order = ToolbarItemOrder.Secondary,
+                Priority = 0
+            };
+
+            item.Clicked += (sender, args) => {
+                Navigation.PushAsync(new ContattiPage());
+            };
+            this.ToolbarItems.Add(item);
+
+
+            ToolbarItem item2 = new ToolbarItem
+            {
+                Text = "Faq",
+                Order = ToolbarItemOrder.Secondary,
+                Priority = 0
+            };
+
+            item2.Clicked += (sender, args) => {
+                Navigation.PushAsync(new FaqPage());
+            };
+            this.ToolbarItems.Add(item2); // "this" refers to a Page object
+
+
+            ToolbarItem item3 = new ToolbarItem
+            {
+                Text = "Logout",
+                IconImageSource = ImageSource.FromFile("example_icon.png"),
+                Order = ToolbarItemOrder.Secondary,
+                Priority = 0
+            };
+            item3.Clicked += async (sender, args) => {
+                bool logout = await Auth.Logout();
+                if (logout)
+                {
+                    Application.Current.Properties["logged"] = "false";
+                    await Application.Current.SavePropertiesAsync();
+                    //Application.Current.Properties.Clear();
+                    await DisplayAlert("Attenzione!", "Il logout è stato eseguito, l'app verrà chiusa!", "Ok");
+                    await System.Threading.Tasks.Task.Delay(1000);
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+
+                }
+                else await DisplayAlert("Attenzione!", "Non è stato possibile effettuare il logout", "Ok");
+
+            };
+            this.ToolbarItems.Add(item3);
         }
 
         protected override void OnAppearing()
